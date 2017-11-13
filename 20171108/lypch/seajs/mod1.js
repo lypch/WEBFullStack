fecdb95fd2bcec233a03353f8e727ddbaa6a49c9
lypch <lypch@ammicdeMacBook-Pro.local>
2017-11-13 08:16:08 +0800

    20171112练习

---
+++ 20171108/lypch/seajs/mod1.js
@@ -0,0 +1,12 @@
+define(function(require,exports,module){
+    //exports.a=12;
+    //exports.b=5;
+
+    //module.exports
+    module.exports={
+        a:12,b:5,
+        show:function(a,b){
+            alert(a+b);
+        }
+    }
+})
