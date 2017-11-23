ac0b5dc9c6445f340132347a1fdb490eba9cd21a
lypch <lypch@ammicdeMacBook-Pro.local>
2017-11-23 21:09:31 +0800

    对runner进行拆解分析，并进行了标注。

--- 20171108/lypch/runner.js
+++ 20171108/lypch/runner.js
@@ -4,13 +4,16 @@ function runner(_gen){
 
     _next();
     function _next(_last_res){
-      var res=gen.next(_last_res);
+      var res=gen.next(_last_res);  //next返回一个generator对象
+      //res是next返回的JSON实例，包含done和value两个属性
 
       if(!res.done){
-        var obj=res.value;
+        var obj=res.value; //value返回的是一个promise对象
+        //也就是说在使用runner的时候，yield返回的必须是一个promise实例
+        //$ajax对象返回的也是一个Promise实例
 
         if(obj.then){
-          obj.then((res)=>{
+          obj.then((res)=>{  //通过Promise实例的Then函数等待异步操作完成
             _next(res);
           }, (err)=>{
             reject(err);
